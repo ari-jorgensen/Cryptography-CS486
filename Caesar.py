01d2842a2f30e918b25d7ca574ad494b11fc0049
ari-jorgensen <32882891+ari-jorgensen@users.noreply.github.com>
2018-08-28 16:17:43 -0700

    fixed shift val

--- Caesar.py
+++ Caesar.py
@@ -1,16 +1,16 @@
 # File: Caesar.py
 # Author: Ariana Jorgensen
 # This program handles the encryption and decryption 
-# of a file using the Caesar cipher. I made the shift
-# a constant of 4, however this would technically
-# work with any shift # as I use mod 26 to ensure
-# that the index remains in the alphabet.
+# of a file using the Caesar cipher. I used the standard
+# shift value of 3 (as reported in the textbook). However,
+# this cipher would work for any shift value, as I use mod 
+# 26 to ensure that the index remains in the alphabet.
 # NOTE: For simplicity, all characters are converted to lowercase
 
 alpha = 'abcdefghijklmnopqrstuvwxyz'
 
 # Function to encrypt file using the 
-# Caesar cipher. Shift = 4
+# Caesar cipher. Shift = 3
 def caesarCipherEncrypt(line):
     caesarLine = ''
     caesarWord = ''
@@ -18,7 +18,7 @@ def caesarCipherEncrypt(line):
         for char in word:
             try:
                 char = char.lower()
-                ind = (alpha.index(char) + 4) % 26
+                ind = (alpha.index(char) + 3) % 26
                 caesarWord += alpha[ind]
             except ValueError:
                 caesarWord += char
@@ -27,7 +27,7 @@ def caesarCipherEncrypt(line):
     return caesarLine
 
 # Function to decrypt file that was encrypted
-# using the Caesar cipher. Shift = 4
+# using the Caesar cipher. Shift = 3
 def caesarCipherDecrypt(line):
     normalLine = ''
     normalWord = ''
@@ -35,7 +35,7 @@ def caesarCipherDecrypt(line):
         for char in word:
             try:
             	char = char.lower()
-                ind = (alpha.index(char) - 4) % 26
+                ind = (alpha.index(char) - 3) % 26
                 normalWord += alpha[ind]
             except ValueError:
                 normalWord += char
